c071498375ee18d64347e6681d610d0f75198486
ROHAN8774 <rohan10bhakte0708@gmail.com>
2024-01-15 20:18:00 +0530

    Assign Roles on Registration

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -4,13 +4,11 @@ using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BulkyBook.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext<ApplicationDbContext>(option =>
-//option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
 
@@ -24,11 +22,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	)
 );
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddScoped<IEmailSender , EmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
